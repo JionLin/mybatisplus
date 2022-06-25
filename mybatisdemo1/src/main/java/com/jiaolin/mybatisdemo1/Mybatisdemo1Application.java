4421556d94acb8a4aaa408b4a4d5353f859fecf0
JionLin <250912986@qq.com>
2022-06-25 15:02:20 +0800

    Initial commit

---
+++ mybatisdemo1/src/main/java/com/jiaolin/mybatisdemo1/Mybatisdemo1Application.java
@@ -0,0 +1,13 @@
+package com.jiaolin.mybatisdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatisdemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatisdemo1Application.class, args);
+    }
+
+}
---
+++ mybatisdemo1/src/test/java/com/jiaolin/mybatisdemo1/Mybatisdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiaolin.mybatisdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatisdemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
