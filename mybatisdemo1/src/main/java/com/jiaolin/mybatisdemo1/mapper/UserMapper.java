2f0e12fb1b5eef5ce89ac35e907f1774c1aaeca6
JionLin <250912986@qq.com>
2022-06-26 09:39:43 +0800

    springboot与mybatis plus 最基本的增删改茶代码。

--- mybatisdemo1/src/main/java/com/jiaolin/mybatisdemo1/mapper/UserMapper.java
+++ mybatisdemo1/src/main/java/com/jiaolin/mybatisdemo1/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.jiaolin.mybatisdemo1.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jiaolin.mybatisdemo1.pojo.User;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author johnny
@@ -9,5 +10,6 @@ import com.jiaolin.mybatisdemo1.pojo.User;
  * @Description
  * @Date 2022/6/25 15:10
  */
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 }
--- mybatisdemo1/src/test/java/com/jiaolin/mybatisdemo1/Mybatisdemo1ApplicationTests.java
+++ mybatisdemo1/src/test/java/com/jiaolin/mybatisdemo1/Mybatisdemo1ApplicationTests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class Mybatisdemo1ApplicationTests {
@@ -19,6 +21,49 @@ class Mybatisdemo1ApplicationTests {
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
     }
+
+
+    @Test
+    public void test2(){
+        User user=new User();
+        user.setAge(18);
+        user.setEmail("xxx.com");
+        user.setName("xz");
+        int value = userMapper.insert(user);
+        System.out.println(value);
+        System.out.println(user.getId());
+    }
+    @Test
+    public void testDeleteMap(){
+        Map<String, Object> map=new HashMap<>();
+        map.put("age", 18);
+        map.put("name", "xz");
+        int value = userMapper.deleteByMap(map);
+        System.out.println(value);
+    }
+
+
+    @Test
+    public void testUpdate(){
+
+        User user=new User();
+        user.setId(3L);
+        user.setAge(22);
+        user.setName("johnny");
+        int value = userMapper.updateById(user);
+        System.out.println(value);
+    }
+
+    @Test
+    public void testByMap(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("age", 18);
+        map.put("name","johnny");
+        List<User> users = userMapper.selectByMap(map);
+        users.forEach(System.out::println);
+    }
+
+
 
 
 }
